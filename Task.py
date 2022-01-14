b558484a10aa56c6b9d3a0e4f8611a230beb7274
Priyarani2021 <86540937+Priyarani2021@users.noreply.github.com>
2022-01-14 19:39:14 +0530

    Add files via upload

--- Task.py
+++ Task.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+listA = []
+# input number of sms
+n = int(input("Enter number of sms : "))
+res=[]
+for i in range(0, n):
+    elm = (input())
+    listA.append(elm) # adding the element
+for j in listA:
+    if j not in res:
+        res.append(j)
+print ("after removing duplicates sms : ",res)
+
+
+# In[ ]:
+
+
+
 
