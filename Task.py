3209a73246c0c5fd9724598999faff19cddd22fa
Priyarani2021 <86540937+Priyarani2021@users.noreply.github.com>
2022-01-14 19:46:21 +0530

    Add files via upload

--- Task.py
+++ Task.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+Vishu works as a personal assistant for a collector. The collector is very strict when it comes to
+duplicate sms. He asks his personal assistant to delete all sms that are duplicates and to keep one
+copy of each. Helo Vishu by writing a program that can tell him what sms to keep and what to
+delete.
+Input
+1. N – number of sms
+2. For each I in N:
+a. Sms subject (one word no special characters)
+Output
+N outputs specifying whether or not to delete the message. Use ‘D’- as a prefix to delete otherwise
+keep it as is
+Example
+N – 5
+Offer
+Urgent
+Minister
+Offer
+Urgent
+Output
+Offer
+Urgent
+Minister
+D-Offer
+D-Urgent
+
+
 # In[13]:
 
 
